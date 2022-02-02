632b55e461cbb7c406e9c3fd7cfeba23eb4a7ef8
Chris Whiting <mpro34@gmail.com>
2022-02-01 22:24:18 -0800

    Add escape key handler, fps counter, and fullscreen option

--- DracoaEngine/main.cpp
+++ DracoaEngine/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <sstream>
 #define GLEW_STATIC
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
@@ -7,6 +7,10 @@
 const char* ENGINE_TITLE = "Dracoa Engine v1.0";
 const int gWindowWidth = 800;
 const int gWindowHeight = 600;
+bool gFullscreen = false;
+
+void glfw_onKey(GLFWwindow* window, int key, int scancode, int action, int mode);
+void showFPS(GLFWwindow* window);
 
 int main()
 {
@@ -23,8 +27,22 @@ int main()
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-    // Create a window with glfw
-    GLFWwindow* pWindow = glfwCreateWindow(gWindowWidth, gWindowHeight, ENGINE_TITLE, nullptr, nullptr);
+    // Create a window with glfw, fullscreen or windowed
+    GLFWwindow* pWindow = nullptr;
+    if (gFullscreen)
+    {
+        GLFWmonitor* pMonitor = glfwGetPrimaryMonitor();
+        const GLFWvidmode* pVmode = glfwGetVideoMode(pMonitor);
+        if (pVmode != nullptr)
+        {
+            pWindow = glfwCreateWindow(pVmode->width, pVmode->height, ENGINE_TITLE, pMonitor, nullptr);
+        }
+    }
+    else
+    {
+        pWindow = glfwCreateWindow(gWindowWidth, gWindowHeight, ENGINE_TITLE, nullptr, nullptr);
+    }
+
     if (pWindow == nullptr)
     {
         std::cerr << "Failed to create GLFW window" << std::endl;
@@ -34,6 +52,8 @@ int main()
 
     // Make the created window current to draw to.
     glfwMakeContextCurrent(pWindow);
+
+    glfwSetKeyCallback(pWindow, glfw_onKey);
 
     glewExperimental = GL_TRUE; // Must set to true in order to use modern opengl
     if (glewInit() != GLEW_OK)
@@ -45,11 +65,56 @@ int main()
 
     while (!glfwWindowShouldClose(pWindow))
     {
+        showFPS(pWindow);
         glfwPollEvents(); // Query the window for keyboard/mouse events
 
+        glClearColor(0.23f, 0.38f, 0.47f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
 
+        glfwSwapBuffers(pWindow); // Allows for double buffering, which eliminates screen tearing
     }
 
     glfwTerminate();
     return 0;
+}
+
+// On a key press, this function is called
+void glfw_onKey(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, GL_TRUE);
+    }
+}
+
+// 
+void showFPS(GLFWwindow* window)
+{
+    static double previousSeconds = 0.0;
+    static int frameCount = 0;
+    double elapsedSeconds;
+    double currentSeconds = glfwGetTime(); // returns the number of seconds since glfw started, as a double.
+
+    elapsedSeconds = currentSeconds - previousSeconds;
+
+    // limit text update 4 times per second
+    if (elapsedSeconds > 0.25)
+    {
+        previousSeconds = currentSeconds;
+        double fps = (double)frameCount / elapsedSeconds;
+        double msPerFrame = 1000.0 / fps;
+
+        // Construct the window title with FPS counts
+        std::ostringstream outs;
+        outs.precision(3);
+        outs << std::fixed
+            << ENGINE_TITLE << "  "
+            << "FPS: " << fps << "  "
+            << "Frame Time: " << msPerFrame << " (ms)";
+
+        glfwSetWindowTitle(window, outs.str().c_str());
+
+        frameCount = 0;
+    }
+    frameCount++;
 }
