31ba71ef8456e958a0277a13b68417def81aa9ad
Chris Whiting <mpro34@gmail.com>
2022-02-02 17:21:26 -0800

    Add vertex and fragment shaders, draw triangle to screen

--- DracoaEngine/main.cpp
+++ DracoaEngine/main.cpp
@@ -7,17 +7,140 @@
 const char* ENGINE_TITLE = "Dracoa Engine v1.0";
 const int gWindowWidth = 800;
 const int gWindowHeight = 600;
-bool gFullscreen = false;
+GLFWwindow* gWindow = nullptr;
+
+// Triangle Vertex Shader (XYZW)
+const GLchar* vertexShaderSrc =
+"#version 330 core\n"
+"layout (location = 0) in vec3 pos;"
+"void main()"
+"{"
+"    gl_Position = vec4(pos.x, pos.y, pos.z, 1.0);"
+"}";
+
+// Triangle Fragment Shader (RGBA)
+const GLchar* fragmentShaderSrc =
+"#version 330 core\n"
+"out vec4 color;"
+"void main()"
+"{"
+"    color = vec4(0.35f, 0.96f, 0.3f, 1.0f);"
+"}";
+
 
 void glfw_onKey(GLFWwindow* window, int key, int scancode, int action, int mode);
 void showFPS(GLFWwindow* window);
+bool initOpenGL();
 
 int main()
+{
+    if (!initOpenGL())
+    {
+        std::cerr << "GLFW initialization failed" << std::endl;
+        return -1;
+    }
+
+    /* begin region - Draw a Triangle */
+    // Each vertiex defined in 3d space: x, y, z
+    GLfloat vertices[] = {
+        0.0f, 0.5f, 0.0f, // Top
+        0.5f, -0.5f, 0.0f, // Right
+        -0.5f, -0.5f, 0.0f, // Left
+    };
+
+    // Send vertices to GPU and store in buffer, vao vs vbo?
+    GLuint vbo, vao;
+    // Create a chunk(buffer) of memory in the gpu for us
+    glGenBuffers(1, &vbo);
+    // Make the vbo the current buffer - 1 active at a time
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    // Pass vertices to created gpu vertex buffer
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    // Create a vertex array object for faster caching
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    // Define the attributes of the vertex data (how should the GPU interpret the data?)
+    // The "stride" determines where in the data array is this attributes data.
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+    glEnableVertexAttribArray(0);
+
+    GLint result;
+    GLchar infoLog[512];
+    // Create, assign, and compile the vertex shader
+    GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vs, 1, &vertexShaderSrc, nullptr);
+    glCompileShader(vs);
+    glGetShaderiv(vs, GL_COMPILE_STATUS, &result);
+    if (!result)
+    {
+        glGetShaderInfoLog(vs, sizeof(infoLog), nullptr, infoLog);
+        std::cout << "Error! Vertex shader failed to compile. " << infoLog << std::endl;
+    }
+
+    // Create, assign, and compile the fragment shader
+    GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fs, 1, &fragmentShaderSrc, nullptr);
+    glCompileShader(fs);
+    glGetShaderiv(fs, GL_COMPILE_STATUS, &result);
+    if (!result)
+    {
+        glGetShaderInfoLog(fs, sizeof(infoLog), nullptr, infoLog);
+        std::cout << "Error! Fragment shader failed to compile. " << infoLog << std::endl;
+    }
+
+    // Create the shader program with all compiled shaders above (vertex and fragment)
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vs);
+    glAttachShader(shaderProgram, fs);
+    glLinkProgram(shaderProgram);
+
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &result);
+    if (!result)
+    {
+        glGetProgramInfoLog(shaderProgram, sizeof(infoLog), nullptr, infoLog);
+        std::cout << "Error! Shader program linker failure. " << infoLog << std::endl;
+    }
+
+    // Once shader program is created, we can free the shader memory before entering the game loop.
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    /* end region */
+
+    // Main loop
+    while (!glfwWindowShouldClose(gWindow))
+    {
+        showFPS(gWindow);
+
+        glfwPollEvents(); // Query the window for keyboard/mouse events
+
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(vao);
+        // Tell opengl to draw triangles from the vao and how many vertices.
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindVertexArray(0);
+
+        glfwSwapBuffers(gWindow); // Allows for double buffering, which eliminates screen tearing
+    }
+
+    // Clean up shader memory
+    glDeleteProgram(shaderProgram);
+    glDeleteVertexArrays(1, &vao);
+    glDeleteBuffers(1, &vbo);
+
+    glfwTerminate();
+    return 0;
+}
+
+bool initOpenGL()
 {
     if (!glfwInit())
     {
         std::cerr << "GLFW initialization failed" << std::endl;
-        return -1;
+        return false;
     }
 
     // Set the min OpenGL version to 3.3
@@ -28,54 +151,30 @@ int main()
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
     // Create a window with glfw, fullscreen or windowed
-    GLFWwindow* pWindow = nullptr;
-    if (gFullscreen)
-    {
-        GLFWmonitor* pMonitor = glfwGetPrimaryMonitor();
-        const GLFWvidmode* pVmode = glfwGetVideoMode(pMonitor);
-        if (pVmode != nullptr)
-        {
-            pWindow = glfwCreateWindow(pVmode->width, pVmode->height, ENGINE_TITLE, pMonitor, nullptr);
-        }
-    }
-    else
-    {
-        pWindow = glfwCreateWindow(gWindowWidth, gWindowHeight, ENGINE_TITLE, nullptr, nullptr);
-    }
-
-    if (pWindow == nullptr)
+    gWindow = glfwCreateWindow(gWindowWidth, gWindowHeight, ENGINE_TITLE, nullptr, nullptr);
+    if (gWindow == nullptr)
     {
         std::cerr << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
-        return -1;
+        return false;
     }
 
     // Make the created window current to draw to.
-    glfwMakeContextCurrent(pWindow);
+    glfwMakeContextCurrent(gWindow);
 
-    glfwSetKeyCallback(pWindow, glfw_onKey);
+    glfwSetKeyCallback(gWindow, glfw_onKey);
 
     glewExperimental = GL_TRUE; // Must set to true in order to use modern opengl
     if (glewInit() != GLEW_OK)
     {
         std::cerr << "GLEW initialization failed" << std::endl;
 
-        return -1;
+        return false;
     }
 
-    while (!glfwWindowShouldClose(pWindow))
-    {
-        showFPS(pWindow);
-        glfwPollEvents(); // Query the window for keyboard/mouse events
+    glClearColor(0.23f, 0.38f, 0.47f, 1.0f);
 
-        glClearColor(0.23f, 0.38f, 0.47f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        glfwSwapBuffers(pWindow); // Allows for double buffering, which eliminates screen tearing
-    }
-
-    glfwTerminate();
-    return 0;
+    return true;
 }
 
 // On a key press, this function is called
